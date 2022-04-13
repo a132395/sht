67bdf40e7625e04ad7055d8687c08aff3888ffff
a132395 <67187730+a132395@users.noreply.github.com>
2022-04-13 16:32:18 +0800

    Update init_db.js

--- init_db.js
+++ init_db.js
@@ -29,6 +29,9 @@ const gotInstance = got.extend({
     },
     prefixUrl: SHTBASE,
     cookieJar : cookieJar,
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+    }
 });
 
 const client = new Client({
